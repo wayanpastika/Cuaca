df22342ba19ecd4b627775f0dab93ff2e479a240
wayanpastika <pastikagede33@gmail.com>
2018-03-26 22:30:15 +0800

    cuaca

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 //import Volumebalok from './src/coba/VolumeBalok';
 //import Header from './src/cuaca/Header';
-import PrakiraanCuaca2 from './src/coba/PrakiraanCuaca2';
+import PrakiraanCuaca from './src/coba/PrakiraanCuaca';
 export default class App extends React.Component {
 
   render() {
     return (
       <View style={styles.ContainerMain}>
-      <PrakiraanCuaca2 />
+      <PrakiraanCuaca />
 
 
       </View>
